89c0fd8adaca48819c999159ca63e8476f2e9f79
mateusbonet <mateusbonet@yahoo.com.br>
2025-07-18 19:24:28 -0300

    feat: Ajustar chamada do serviço de pagamento

--- src/main/java/com/order/wise/application/usecase/ProcessOrderUseCase.java
+++ src/main/java/com/order/wise/application/usecase/ProcessOrderUseCase.java
@@ -1,8 +1,6 @@
 package com.order.wise.application.usecase;
 
 import com.order.wise.domain.Order;
-import com.order.wise.domain.enums.StatusEnum;
-import com.order.wise.gateway.OrderGateway;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -14,16 +12,10 @@ public class ProcessOrderUseCase {
 
     private final CreateOrderUseCase createOrderUseCase;
     private final LowStockUseCase lowStockUseCase;
-    private final PaymentUseCase paymentUseCase;
-    private final OrderGateway orderGateway;
 
     public void execute(Order order) {
         log.info("Processing order: {}", order);
         Order orderSaved = createOrderUseCase.createOrder(order);
         lowStockUseCase.execute(orderSaved);
-        Order statusAfterLowStock = orderGateway.findById(order.getId());
-        if (statusAfterLowStock.getStatus() == StatusEnum.OPEN) {
-            paymentUseCase.execute(orderSaved);
-        }
     }
 }
